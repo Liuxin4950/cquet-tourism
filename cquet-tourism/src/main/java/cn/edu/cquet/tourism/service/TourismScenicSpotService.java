d4825ad21a028c63c60839ae8d7ab1238da0d4e8
liuxin <1371149587@qq.com>
2025-11-05 11:21:49 +0800

    feat(mapper): 添加旅游景点数据访问层接口和旅游景点服务层。 新增TourismScenicSpotMapper接口，继承BaseMapper实现基础CRUD操作

---
+++ cquet-tourism/src/main/java/cn/edu/cquet/tourism/service/TourismScenicSpotService.java
@@ -0,0 +1,10 @@
+package cn.edu.cquet.tourism.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+
+import cn.edu.cquet.tourism.domain.TourismScenicSpot;
+
+public interface TourismScenicSpotService extends IService<TourismScenicSpot> {
+
+   
+}
